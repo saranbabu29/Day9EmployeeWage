bad97a51aeeb5b2aa3f6d9040cd1f8b05ec4dc56
saranbabu29 <sharanraj982@gmail.com>
2022-08-14 16:23:31 +0530

    Present or Not

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 0;
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            Console.WriteLine("------------------------------------");
+
+            Random random = new Random();
+            int value = random.Next(0, 2);
+
+            if (value == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
