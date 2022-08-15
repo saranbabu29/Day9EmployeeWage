9522a5d5c23f015960dc2603771a6ef2d50f2437
saranbabu29 <sharanraj982@gmail.com>
2022-08-15 18:59:48 +0530

    Wage of Mul Comp

--- Program.cs
+++ Program.cs
@@ -6,47 +6,36 @@ namespace EmployeeWage
     {
         public const int isFullTime = 0;
         public const int isPartTime = 1;
-        public const int wagePerHour = 20;
-        public const int days = 20;
-        public const int maxHours = 100;
 
-        public static int computeWage()
+        public static int computeWage(String company, int wagePerHour, int days)
         {
             int empHr = 0;
             int wage = 0, wagePerDay = 0;
 
-            Console.WriteLine("Welcome to Employee Wage Computation");
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine("");
-
-            if (maxHours == 100 && days == 20)
+            Random random = new Random();
+            int value = random.Next(0, 2);
+            switch (value)
             {
+                case isFullTime:
+                    empHr = 8;
+                    break;
+                case isPartTime:
+                    empHr = 4;
+                    break;
+                default:
+                    empHr = 0;
+                    break;
 
-
-                Random random = new Random();
-                int value = random.Next(0, 2);
-                switch (value)
-                {
-                    case isFullTime:
-                        empHr = 8;
-                        break;
-                    case isPartTime:
-                        empHr = 4;
-                        break;
-                    default:
-                        empHr = 0;
-                        break;
-
-                }
-                wagePerDay = empHr * wagePerHour;
-                wage = wagePerDay * days;
-                Console.WriteLine("Employee Wage is " + wage);
-                return wage;
             }
+            wagePerDay = empHr * wagePerHour;
+            wage = wagePerDay * days;
+            Console.WriteLine("Employee Wage of " + company + "is " + wage);
+            return wage;
         }
         static void Main(string[] args)
         {
-            computeWage();
+            computeWage("Reliance", 20, 20);
+            computeWage("TATA", 40, 20);
         }
     }
 }
